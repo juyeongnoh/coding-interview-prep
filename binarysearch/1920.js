5abdfcbb0803c0507dbd5cf3689c5a2904be2f29
juyeongnoh <juyeongnoh@gmail.com>
2023-11-28 13:50:41 +0900

    done 14501 (퇴사)

---
+++ binarysearch/1920.js
@@ -0,0 +1,7 @@
+/**
+ * 수 찾기
+ * https://www.acmicpc.net/problem/1920
+ */
+function binarySearch(arr, left, mid, right, target) {}
+
+let arr = [1, 4, 6, 7, 8, 23, 76];
---
+++ dynamicprogramming/14501.js
@@ -0,0 +1,23 @@
+/**
+ * 퇴사
+ * https://www.acmicpc.net/submit/14501
+ */
+
+const fs = require("fs");
+const input = fs.readFileSync("dev/stdin").toString().split("\n");
+
+let n = Number(input[0]);
+let arr = input.slice(1).map((el) => el.split(" ").map(Number));
+
+const dp = new Array(n).fill(0);
+
+for (let i = 0; i < n; i++) {
+  const [duration, profit] = arr[i];
+  if (i + duration > n) continue;
+  dp[i] += profit;
+  for (let j = i + duration; j < n; j++) {
+    console.log(dp[j], dp[i]);
+    dp[j] = Math.max(dp[j], dp[i]);
+  }
+}
+console.log(Math.max(...dp));
