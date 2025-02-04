94a4329ba20fe94b6a2218f8a55deac316fc422c
Juyeong "James" Noh <juyeongnoh@gmail.com>
2025-02-04 19:53:09 +0900

    retry: 2606

--- dfs/2606.js
+++ dfs/2606.js
@@ -1,40 +1,42 @@
-/**
- * 바이러스
- * https://www.acmicpc.net/problem/2606
- */
-const input = require("fs")
-  .readFileSync("dev/stdin")
-  .toString()
-  .trim()
-  .split("\n");
-const com = Number(input[0]);
+// 입력값 가공
+const fs = require("fs");
+const input = fs.readFileSync("dev/stdin").toString().split("\n");
 
-input.splice(0, 2);
+const computerCount = Number(input[0]);
+const nodes = Number(input[1]);
+const pairs = input.slice(2).map((pair) => pair.split(" ").map(Number));
 
-const visited = Array(com + 1).fill(false);
-const pairs = input.map((el) => el.split(" ").map(Number));
+// 입력 데이터로 그래프 만들기
+const graph = Array.from({ length: computerCount + 1 }).map(() => []);
 
-const graph = new Array(com + 1).fill().map(() => []);
+for (let i = 0; i < nodes; i++) {
+  const [node1, node2] = pairs[i];
+  graph[node1].push(node2);
+  graph[node2].push(node1);
+}
 
-pairs.forEach((pair) => {
-  const [first, second] = pair;
-  graph[first].push(second);
-  graph[second].push(first);
-});
+// 1번 그래프와 연결된 노드 수
+let nodeCount = 0;
 
-const stack = [];
+// 지나온 노드 보관하는 스택
+const visited = [];
 
-function dfs(v) {
-  visited[v] = true;
-  stack.push(v);
+// DFS 순회 함수
+function dfs(graph, currentNode, visited) {
+  // 노드 방문 시 nodeCount 증가하고 방문 처리
+  nodeCount++;
+  visited.push(currentNode);
 
-  for (const i of graph[v]) {
-    if (!visited[i]) {
-      dfs(i);
-    }
+  // 인접 노드 방문
+  for (const nextNode of graph[currentNode]) {
+    // 다음 노드가 이미 방문한 노드라면 방문 생략
+    if (visited.includes(nextNode)) continue;
+
+    // 다음 노드 방문
+    dfs(graph, nextNode, visited);
   }
 }
 
-dfs(1);
+dfs(graph, 1, visited);
 
-console.log(stack.length - 1);
+console.log(nodeCount - 1);
